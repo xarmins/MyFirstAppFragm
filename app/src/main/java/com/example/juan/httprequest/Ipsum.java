04776c1ad4d258fe51c04b6b109699d690b571a9
inftel01 <xarmins@gmail.com>
2017-03-30 14:53:46 +0200

    todo

---
+++ app/src/main/java/com/example/juan/httprequest/Ipsum.java
@@ -0,0 +1,17 @@
+package com.example.juan.httprequest;
+
+/**
+ * Created by Juan on 25/03/2017.
+ */
+
+class Ipsum {
+    static String[] Headlines = {
+            "Barça 1 - 0 Real Sociedad",
+            "Real Madrid 0 - 1 Granada"
+    };
+
+    static String[] Articles = {
+            "Partido 1\n\nEl Barça ganó gracias a la gran actuación de portero alemán",
+            "Partido 2\n\nEl Real Madrid no pasó del empate contra el farolillo de la Liga"
+    };
+}
